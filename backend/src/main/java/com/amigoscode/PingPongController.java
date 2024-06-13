22a34087b598b74b61e123924a834e419c91fb1a
Kevin Bui <buikevin96@gmail.com>
2024-06-13 12:20:01 -0400

    CD Workflow with new Controller
    Created backend-cd

---
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -0,0 +1,12 @@
+package com.amigoscode;
+
+@RestController
+public class PingPongController {
+
+    record PingPong(String result){}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong() {
+        return new PingPong("Pong");
+    }
+}
