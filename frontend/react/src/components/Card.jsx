fbac6816f8b1ac6cfb337c10b77daa8d1b67ded7
Kevin Bui <buikevin96@gmail.com>
2024-06-18 17:42:28 -0400

    Added delete functionality

--- frontend/react/src/components/Card.jsx
+++ frontend/react/src/components/Card.jsx
@@ -9,52 +9,124 @@ import {
     Stack,
     Tag,
     useColorModeValue,
+    Button,
+    useDisclosure,
+    AlertDialogOverlay,
+    AlertDialogContent,
+    AlertDialogHeader,
+    AlertDialogBody, AlertDialogFooter, AlertDialog,
 } from '@chakra-ui/react';
+import React from "react";
+import {deleteCustomer} from "../services/client.js";
+import {errorNotification, successNotification} from "../services/notification.js";
 
-export default function CardWithImage({id, name, email, age, gender, imageNumber}) {
+export default function CardWithImage({id, name, email, age, gender, imageNumber, fetchCustomers}) {
     const randomUserGender = gender === "MALE" ? "men" : "women";
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const cancelRef = React.useRef()
 
-    return (
-        <Center py={6}>
-            <Box
-                maxW={'300px'}
+    return <Center py={6}>
+        <Box
+            maxW={'300px'}
+            w={'full'}
+            bg={useColorModeValue('white', 'gray.800')}
+            boxShadow={'2xl'}
+            rounded={'md'}
+            overflow={'hidden'}>
+            <Image
+                h={'120px'}
                 w={'full'}
-                bg={useColorModeValue('white', 'gray.800')}
-                boxShadow={'2xl'}
-                rounded={'md'}
-                overflow={'hidden'}>
-                <Image
-                    h={'120px'}
-                    w={'full'}
+                src={
+                    'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+                }
+                objectFit={'cover'}
+            />
+            <Flex justify={'center'} mt={-12}>
+                <Avatar
+                    size={'xl'}
                     src={
-                        'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+                        `https://randomuser.me/api/portraits/${randomUserGender}/${imageNumber}.jpg`
                     }
-                    objectFit={'cover'}
+                    alt={'Author'}
+                    css={{
+                        border: '2px solid white',
+                    }}
                 />
-                <Flex justify={'center'} mt={-12}>
-                    <Avatar
-                        size={'xl'}
-                        src={
-                            `https://randomuser.me/api/portraits/${randomUserGender}/${imageNumber}.jpg`
-                        }
-                        alt={'Author'}
-                        css={{
-                            border: '2px solid white',
-                        }}
-                    />
-                </Flex>
+            </Flex>
 
-                <Box p={6}>
-                    <Stack spacing={2} align={'center'} mb={5}>
-                        <Tag borderRadius={"full"}>{id}</Tag>
-                        <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
-                            {name}
-                        </Heading>
-                        <Text color={'gray.500'}>{email}</Text>
-                        <Text color={'gray.500'}>Age {age} | {gender}</Text>
-                    </Stack>
-                </Box>
+            <Box p={6}>
+                <Stack spacing={2} align={'center'} mb={5}>
+                    <Tag borderRadius={"full"}>{id}</Tag>
+                    <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
+                        {name}
+                    </Heading>
+                    <Text color={'gray.500'}>{email}</Text>
+                    <Text color={'gray.500'}>Age {age} | {gender}</Text>
+                </Stack>
             </Box>
-        </Center>
-    );
+
+            <Stack m={8}>
+                <Button
+                    mt={8}
+                    bg={"red.400"}
+                    color={'white'}
+                    rounded={'full'}
+                    _hover={{
+                        transform: 'translateY(-2px)',
+                        boxShadow: 'lg'
+                    }}
+                    _focus={{
+                        bg: 'grey.500'
+                    }}
+                    onClick={onOpen}
+                >
+                    Delete
+                </Button>
+
+                <AlertDialog
+                    isOpen={isOpen}
+                    leastDestructiveRef={cancelRef}
+                    onClose={onClose}
+                >
+                    <AlertDialogOverlay>
+                        <AlertDialogContent>
+                            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                                Delete Customer
+                            </AlertDialogHeader>
+
+                            <AlertDialogBody>
+                                Are you sure? You can't undo this action afterwards.
+                            </AlertDialogBody>
+
+                            <AlertDialogFooter>
+                                <Button ref={cancelRef} onClick={onClose}>
+                                    Cancel
+                                </Button>
+                                <Button colorScheme='red' onClick={() => {
+                                    deleteCustomer(id).then(res => {
+                                        console.log(res)
+                                        successNotification(
+                                            "Customer Deleted",
+                                            `${name} was successfully deleted`
+                                        )
+                                        fetchCustomers();
+                                    }).catch(err => {
+                                        console.log(err);
+                                        errorNotification(
+                                            err.code,
+                                            err.response.data.message
+                                        )
+                                    }).finally(() => {
+                                        onClose()
+                                    })
+                                }} ml={3}>
+                                    Delete
+                                </Button>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialogOverlay>
+                </AlertDialog>
+            </Stack>
+        </Box>
+    </Center>;
 }
