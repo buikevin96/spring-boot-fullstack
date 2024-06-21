dd7bb931818a48635beff7840d863fc75043a147
Kevin Bui <buikevin96@gmail.com>
2024-06-21 18:00:48 -0400

    Created and implemented AuthenticationFilter

--- backend/src/main/java/com/amigoscode/jwt/JWTUtil.java
+++ backend/src/main/java/com/amigoscode/jwt/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.amigoscode.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -43,7 +44,33 @@ public class JWTUtil {
         return token;
     }
 
+    public String getSubject(String token){
+        Claims claims = getClaims(token);
+        return claims.getSubject();
+    }
+
+    private Claims getClaims(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims;
+    }
+
     private Key getSigningKey(){
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public boolean isTokenValid(String jwt, String username) {
+        String subject = getSubject(jwt);
+        return subject.equals(username) && !isTokenExpired(jwt);
+    }
+
+    private boolean isTokenExpired(String jwt) {
+        java.util.Date today = java.util.Date.from(
+                Instant.now()
+        );
+        return getClaims(jwt).getExpiration().before(today);
     }
 }
