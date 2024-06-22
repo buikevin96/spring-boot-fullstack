55eae219d98630ad7cabfe158c9dd094e0dff320
Kevin Bui <buikevin96@gmail.com>
2024-06-22 16:17:33 -0400

    Created Login Authentication

--- backend/src/main/java/com/amigoscode/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/amigoscode/security/SecurityFilterChainConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,8 +32,12 @@ public class SecurityFilterChainConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/api/v1/customers")
+                .requestMatchers(
+                        HttpMethod.POST,
+                        "/api/v1/customers",
+                        "/api/v1/auth/login")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
