c7ddba686d22181f0955bbf2c2675d35e081461e
Kevin Bui <buikevin96@gmail.com>
2024-07-11 14:14:09 -0400

    Initiated Application Skeleton

--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
-import {MyFirstComponent} from "./my-first-component/my-first.component";
 import {FormsModule} from "@angular/forms";
+import {AppRoutingModule} from "./app-routing.module";
+import {InputTextModule} from "primeng/inputtext";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyFirstComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
